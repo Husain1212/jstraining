850e9837dce1ba89d3211d416c08006d3615ad9e
Husain1212 <taiyabhusainparihar@gmail.com>
2023-02-06 20:25:23 +0530

    my first commit

---
+++ excersice6.js
@@ -0,0 +1,11 @@
+// let num = Number(prompt("please enter number?"));
+
+// for(i = 1; i <= num; i++){
+//     console.log(i)
+//     console.log("hello world")
+// }
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log("Even", i);
+  }
+}
